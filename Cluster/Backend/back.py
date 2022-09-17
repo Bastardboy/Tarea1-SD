30ce22f0d13aa5715fc954c63b0a9891efbbe684
Basty3lol <bastian.castro@mail.udp.cl>
2022-09-17 18:49:22 -0300

    ctm

---
+++ Cluster/Backend/back.py
@@ -0,0 +1,48 @@
+import grpc
+from concurrent import futures
+import proto_message_pb2 as pb2
+import proto_message_pb2_grpc as pb2_grpc
+from time import sleep
+from routes import querys
+
+class SearchService(pb2_grpc.SearchServicer):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def GetServerResponse(self, request, context):
+        item = []
+        response = []
+        message = request.message
+        result = f'"{message}" '
+        cursor.execute("SELECT * FROM items;")
+        query_res = cursor.fetchall()
+        for row in query_res:
+            if message in row[1]:
+                item.append(row)
+        for i in item:
+            result = dict()
+            result['id'] = i[0]
+            result['title']= i[1]
+            result['description'] = i[2]
+            result['keyword']= i[3]
+            result['url']= i[4]
+            response.append(result)
+        
+        print(pb2.SearchResults(product=response))
+        return pb2.SearchResults(product=response)
+
+def serve():
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_SearchServicer_to_server(SearchService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    sleep(20)
+    conn = querys.init_db()
+    cursor = conn.cursor()
+    serve()
